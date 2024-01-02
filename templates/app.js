d9c56f0d28a3e00088a8559161ee41d82e579f34
Andres Esteban Alzate <56479414+Esteban191@users.noreply.github.com>
2024-01-02 01:33:28 -0500

    Add files via upload
    VERSION 1 PAGINA WEB

---
+++ templates/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutSection = document.getElementById('about');
+
+    aboutSection.addEventListener('click', function() {
+        aboutSection.style.backgroundColor = '#e74c3c'; // Cambia el color al hacer clic
+    });
+});
